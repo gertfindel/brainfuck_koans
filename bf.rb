c2de2ec88875b47e0e9b45c4e52a44156780885c
Gert Findel <gert@findel.cl>
2011-12-03 18:33:42 -0300

    some mini examples.. lessons have to be developed yet

--- bf.rb
+++ bf.rb
@@ -1,62 +1,69 @@
-class Bf
-def initialize
-  @a=[0]*30000
-  @b=[]
-  @p=0
-  @x=0
+#!/usr/bin/ruby1.9.1
+# Brainfuck Interpreter
+# (C) Sebastian Kaspari 2009
+# Usage: bf.rb [FILE]
+ 
+# This code is much much faster than the last one, which did
+# a lot of unnecessary loops which could also lead to bugs
+#
+# There's now a bracket map which stores starting and ending
+# brackets for direct "jumps", that should speed up things too
+ 
+require 'rubygems'
+require "highline/system_extensions"
+include HighLine::SystemExtensions
+ 
+msec = Time.now.to_f * 1000;
+ 
+code = ARGF.read
+le = code.length
+cp = -1 # code pointer
+p = 0 # cell pointer
+c = [0] # cells
+bm = {} # bracket map, jump directly to matching brackets! :)
+bc = 0 # bracket counter
+s = [] # bracket stack
+ccp = 0 # code pointer for cleaned code
+ 
+# valid commands
+commands = [">", "<", "+", "-", ".", "[", "]", ","]
+ 
+cleaned = [] # code goes here
+ 
+# Only select valid commands and build bracket map
+# Now there's also some time for bracket syntax checking :)
+until (cp+=1) == le
+  case code[cp]
+    when ?[ then s.push(ccp) && bc += 1
+    when ?] then (bm[s.pop] = ccp) && bc -= 1
+  end
+  bc < 0 && puts("Ending Bracket without opening, mismatch at #{cp}") && exit
+  commands.include? code[cp] && (cleaned.push code[cp]) && ccp += 1
 end
-
-def r
-  @p+=1 if !@j
-end
-
-def l
-  @p-=1 if !@j
-end
-
-def i
-  @a[@p]+=1 if !@j
-end
-
-def d
-  @a[@p]-=1 if !@j
-end
-
-def o
-  print @a[@p].chr if !@j
-end
-
-def n
-  @a[@p] = $stdin.getc if !@j
-end
-
-def j
-  @x+=1
-  if @a[@p]==0
-    @j=true
-  else
-    @b[@x]=$i-1
+ 
+!s.empty? && puts("Opening Bracket without closing, mismatch at #{s.pop}") && exit
+ 
+# reset defaults
+cp = -1
+ 
+# now lets interprete the code :)
+until (cp+=1) == le
+  case cleaned[cp]
+    when ?> then (p += 1) && c[p].nil? && c[p] = 0
+    when ?< then p <= 1 ? p = 0 : p -= 1
+    when ?+ then c[p] <= 254 ? c[p] += 1 : c[p] = 0
+    when ?- then c[p] >= 1 ? c[p] -= 1 : c[p] = 255
+    when ?[ then c[p] == 0 && cp = bm[cp]
+    when ?] then c[p] != 0 && cp = bm.key(cp)
+    when ?. then print c[p].chr
+    when ?, then c[p] = get_character.to_i
   end
 end
-
-def e
-  if @j
-    @j=false
-  else
-    $i=@b[@x]
-  end
-  @x-=1
-end
-
-def b
-  10.times {|a| print @a[a].chr}
-end
-end
-
-b=Bf.new
-$i=0
-p=$<.readlines.join.tr('^><+\-.,[]#','').tr('><+\-.,[]#', 'rlidonjeb')
-while $i<p.size
-  b.send(p[$i].chr)
-  $i+=1
-end
+ 
+# and finally some benchmarking
+ 
+runtime = Time.now.to_f * 1000 - msec
+ 
+puts ""
+puts ""
+puts "#{runtime} msecs [Used #{c.length} cells]"
